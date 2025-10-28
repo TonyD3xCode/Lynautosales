fe0f1d106a8f1d55ba598812974fa089653c9934
TonyD3xCode <anthony-1701@hotmail.com>
2025-10-28 17:49:14 -0500

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,11 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
   },
+  localePath: path.resolve('./src/locales'),
+  defaultNS: 'common',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
