8c1185234f3a689396488faf14944b563f185e3b
TonyD3xCode <anthony-1701@hotmail.com>
2025-10-28 17:59:56 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ function App({ Component, pageProps }) {
     <>
       <Head>
         <title>LYN AutoSales</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </>
